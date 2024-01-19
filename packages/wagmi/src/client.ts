2b46c31c1c2f75c845682f8967fd05ba89a76c98
Alex <hellhellpnick@gmail.com>
2024-01-19 18:49:41 +0200

    upd

--- packages/wagmi/src/client.ts
+++ packages/wagmi/src/client.ts
@@ -162,31 +162,31 @@ export class Web3Modal extends Web3ModalScaffold {
         await connect({ connector, chainId })
       },
 
-      // checkInstalled: ids => {
-      //   const eip6963Connectors = this.getConnectors().filter(c => c.type === 'ANNOUNCED')
-      //   const injectedConnector = this.getConnectors().find(c => c.type === 'INJECTED')
+      checkInstalled: ids => {
+        const eip6963Connectors = this.getConnectors().filter(c => c.type === 'ANNOUNCED')
+        const injectedConnector = this.getConnectors().find(c => c.type === 'INJECTED')
 
-      //   if (!ids) {
-      //     return Boolean(window.ethereum)
-      //   }
+        if (!ids) {
+          return Boolean(window.ethereum)
+        }
 
-      //   if (eip6963Connectors.length) {
-      //     const installed = ids.some(id => eip6963Connectors.some(c => c.info?.rdns === id))
-      //     if (installed) {
-      //       return true
-      //     }
-      //   }
+        if (eip6963Connectors.length) {
+          const installed = ids.some(id => eip6963Connectors.some(c => c.info?.rdns === id))
+          if (installed) {
+            return true
+          }
+        }
 
-      //   if (injectedConnector) {
-      //     if (!window?.ethereum) {
-      //       return false
-      //     }
+        if (injectedConnector) {
+          if (!window?.ethereum) {
+            return false
+          }
 
-      //     return ids.some(id => Boolean(window.ethereum?.[String(id)]))
-      //   }
+          return ids.some(id => Boolean(window.ethereum?.[String(id)]))
+        }
 
-      //   return false
-      // },
+        return false
+      },
 
       disconnect: async () => {
         await disconnect()
